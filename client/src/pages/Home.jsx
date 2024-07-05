ce5fec3304ddf7d2994916eb00e1fa46f9f476ec
vasu03 <vasurmakadia@gmail.com>
2024-07-05 22:58:05 +0530

    [ update ] :: Home page completed.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,15 +6,18 @@ import React from "react";
 
 
 // Importing custom components
-
+import AppLayout from "../components/layoutComponents/AppLayout";
 
 // Creating the Home Page
 const Home = () => {
 	// JSX to render the Home Page
 	return (
-		<div>Home</div>
+		<div className="flex flex-col text-center items-center justify-center w-full h-full text-lg p-3">
+			<p>Welcome to Talk Tide...</p>
+			<p>Select a conversation to start chatting with your friends....</p>
+		</div>
 	);
 };
 
-// Exporting the Page
-export default Home;
+// Exporting the Page with the Layout component
+export default AppLayout()(Home);
